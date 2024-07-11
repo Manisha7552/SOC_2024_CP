40bf5b3250c4f19491b18431c0055153f7e993f8
Manisha7552 <manisha.sahu2003@gmail.com>
2024-07-11 16:47:29 +0530

    Update w2_string_compression.cpp

--- w2_string_compression.cpp
+++ w2_string_compression.cpp
@@ -1,35 +1,29 @@
-class Solution {
-public:
+ class Solution {
+  public:
     int compress(vector<char>& chars) {
         int c= chars.size();
         vector<char> y(0);
         
+        
         int k=1;int j;
-        for(int i=0;i<c-1;i++){
-            if(chars[i]==chars[i+1]){
+        for(int i=1;i<=c;i++){
+            if(i<c && chars[i]==chars[i-1] ){
                 k++;
 
             }
             else{
-                if(k==1){
-                    y.push_back(chars[i]);
-                    
+                y.push_back(chars[i-1]);
+                if(k>1){
+                     string temp=to_string(k);
+                    for(char ch:temp){
+                        y.push_back(ch);
+                    }
                 }
-                else{
-                     y.push_back(chars[i]);
-                    
-                    string f=to_string(k);
-                    for(char ch:f){
-                     y.push_back(ch);
-                     
-                     }
-                     
-                    k=1;
-                    
-
-                }
-
+                k=1;
             }
+
+                        
+   
 
         }
         j=y.size();
